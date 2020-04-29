ebfcbd787cf897704c937b0ea104b1042049e960
Strangerke <arnaud.boutonne@gmail.com>
2020-04-29 16:49:57 +0200

    CLUE: Remove useless macros in tcdata.h

--- engines/clue/data/objstd/tcdata.h
+++ engines/clue/data/objstd/tcdata.h
@@ -25,208 +25,104 @@ namespace Clue {
 #define DB_tcMain_OFFSET                    0
 #define DB_tcMain_SIZE                  10000
 
-
-#define Const_tcTALK_ABILITY                4
+#define Const_tcACCESS_BIT                  4
+#define Const_tcCHAINED_TO_ALARM            1
+#define Const_tcCHAINED_TO_POWER            2
+#define Const_tcHORIZ_VERT_BIT              0
 #define Const_tcIN_PROGRESS_BIT             5
 #define Const_tcLOCK_UNLOCK_BIT             2
-#define Const_tcTALK_JOB_OFFER              0
 #define Const_tcON_OFF                      6
-#define Const_tcCAR_PICT_OFFSET            24
-#define Const_tcCHAINED_TO_ALARM            1
-#define Const_tcHORIZ_VERT_BIT              0
-#define Const_tcTAKE_BIT                    3
-#define Const_tcALARM_PICT_OFFSET          25
-#define Const_tcTALK_PRISON_BEFORE          2
-#define Const_tcACCESS_BIT                  4
-#define Const_tcTALK_YOUR_JOB               1
 #define Const_tcOPEN_CLOSE_BIT              1
-#define Const_tcNO_CHAINING                 0
+#define Const_tcTAKE_BIT                    3
+#define Const_tcTALK_JOB_OFFER              0
 #define Const_tcTOOL_OPENS                  1
-#define Const_tcCHAINED_TO_POWER            2
-#define Const_tcSYMBOL_PICT_SIZE           16
-#define Const_tcPOWER_PICT_OFFSET          26
-
 
 #define Relation_hasClock      1
-#define hasClockP(l,r,p,c)     AskP   (dbGetObject (l), 1, dbGetObject (r), p, c)
-#define hasClock(l,r)          AskP   (dbGetObject (l), 1, dbGetObject (r), NO_PARAMETER, CMP_NO)
 #define hasClockAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 1, BuildObjectList); }
-#define hasClockSetP(l,r,p)    SetP   (dbGetObject (l), 1, dbGetObject (r), p)
-#define hasClockSet(l,r)       SetP   (dbGetObject (l), 1, dbGetObject (r), NO_PARAMETER)
-#define hasClockUnSet(l,r)     UnSet  (dbGetObject (l), 1, dbGetObject (r))
-#define hasClockGet(l,r)       GetP   (dbGetObject (l), 1, dbGetObject (r))
+#define hasClockGet(l, r)       GetP(dbGetObject(l), 1, dbGetObject(r))
 
 #define Relation_ClockTimer      2
-#define ClockTimerP(l,r,p,c)     AskP   (dbGetObject (l), 2, dbGetObject (r), p, c)
-#define ClockTimer(l,r)          AskP   (dbGetObject (l), 2, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define ClockTimerAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 2, BuildObjectList); }
 #define ClockTimerSetP(l,r,p)    SetP   (dbGetObject (l), 2, dbGetObject (r), p)
-#define ClockTimerSet(l,r)       SetP   (dbGetObject (l), 2, dbGetObject (r), NO_PARAMETER)
-#define ClockTimerUnSet(l,r)     UnSet  (dbGetObject (l), 2, dbGetObject (r))
 #define ClockTimerGet(l,r)       GetP   (dbGetObject (l), 2, dbGetObject (r))
 
 #define Relation_StairConnects      3
-#define StairConnectsP(l,r,p,c)     AskP   (dbGetObject (l), 3, dbGetObject (r), p, c)
-#define StairConnects(l,r)          AskP   (dbGetObject (l), 3, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define StairConnectsAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 3, BuildObjectList); }
-#define StairConnectsSetP(l,r,p)    SetP   (dbGetObject (l), 3, dbGetObject (r), p)
-#define StairConnectsSet(l,r)       SetP   (dbGetObject (l), 3, dbGetObject (r), NO_PARAMETER)
-#define StairConnectsUnSet(l,r)     UnSet  (dbGetObject (l), 3, dbGetObject (r))
 #define StairConnectsGet(l,r)       GetP   (dbGetObject (l), 3, dbGetObject (r))
 
 #define Relation_has      4
-#define hasP(l,r,p,c)     AskP   (dbGetObject (l), 4, dbGetObject (r), p, c)
 #define has(l,r)          AskP   (dbGetObject (l), 4, dbGetObject (r), NO_PARAMETER, CMP_NO)
 #define hasAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 4, BuildObjectList); }
-#define hasSetP(l,r,p)    SetP   (dbGetObject (l), 4, dbGetObject (r), p)
-#define hasSet(l,r)       SetP   (dbGetObject (l), 4, dbGetObject (r), NO_PARAMETER)
-#define hasUnSet(l,r)     UnSet  (dbGetObject (l), 4, dbGetObject (r))
 #define hasGet(l,r)       GetP   (dbGetObject (l), 4, dbGetObject (r))
+#define hasSet(l,r)       SetP   (dbGetObject (l), 4, dbGetObject (r), NO_PARAMETER)
+#define hasSetP(l,r,p)    SetP   (dbGetObject (l), 4, dbGetObject (r), p)
+#define hasUnSet(l,r)     UnSet  (dbGetObject (l), 4, dbGetObject (r))
 
-#define Relation_knows      5
-#define knowsP(l,r,p,c)     AskP   (dbGetObject (l), 5, dbGetObject (r), p, c)
+//#define Relation_knows      5
 #define knows(l,r)          AskP   (dbGetObject (l), 5, dbGetObject (r), NO_PARAMETER, CMP_NO)
 #define knowsAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 5, BuildObjectList); }
-#define knowsSetP(l,r,p)    SetP   (dbGetObject (l), 5, dbGetObject (r), p)
-#define knowsSet(l,r)       SetP   (dbGetObject (l), 5, dbGetObject (r), NO_PARAMETER)
-#define knowsUnSet(l,r)     UnSet  (dbGetObject (l), 5, dbGetObject (r))
-#define knowsGet(l,r)       GetP   (dbGetObject (l), 5, dbGetObject (r))
+#define knowsSet(l, r)       SetP(dbGetObject(l), 5, dbGetObject(r), NO_PARAMETER)
 
-#define Relation_likes_to_be      6
-#define likes_to_beP(l,r,p,c)     AskP   (dbGetObject (l), 6, dbGetObject (r), p, c)
-#define likes_to_be(l,r)          AskP   (dbGetObject (l), 6, dbGetObject (r), NO_PARAMETER, CMP_NO)
+//#define Relation_likes      6
 #define likes_to_beAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 6, BuildObjectList); }
-#define likes_to_beSetP(l,r,p)    SetP   (dbGetObject (l), 6, dbGetObject (r), p)
-#define likes_to_beSet(l,r)       SetP   (dbGetObject (l), 6, dbGetObject (r), NO_PARAMETER)
-#define likes_to_beUnSet(l,r)     UnSet  (dbGetObject (l), 6, dbGetObject (r))
-#define likes_to_beGet(l,r)       GetP   (dbGetObject (l), 6, dbGetObject (r))
 
-#define Relation_join      7
-#define joinP(l,r,p,c)     AskP   (dbGetObject (l), 7, dbGetObject (r), p, c)
+//#define Relation_join      7
 #define join(l,r)          AskP   (dbGetObject (l), 7, dbGetObject (r), NO_PARAMETER, CMP_NO)
 #define joinAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 7, BuildObjectList); }
-#define joinSetP(l,r,p)    SetP   (dbGetObject (l), 7, dbGetObject (r), p)
 #define joinSet(l,r)       SetP   (dbGetObject (l), 7, dbGetObject (r), NO_PARAMETER)
 #define joinUnSet(l,r)     UnSet  (dbGetObject (l), 7, dbGetObject (r))
-#define joinGet(l,r)       GetP   (dbGetObject (l), 7, dbGetObject (r))
 
-#define Relation_joined_by      8
-#define joined_byP(l,r,p,c)     AskP   (dbGetObject (l), 8, dbGetObject (r), p, c)
-#define joined_by(l,r)          AskP   (dbGetObject (l), 8, dbGetObject (r), NO_PARAMETER, CMP_NO)
+//#define Relation_joined_by      8
 #define joined_byAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 8, BuildObjectList); }
-#define joined_bySetP(l,r,p)    SetP   (dbGetObject (l), 8, dbGetObject (r), p)
 #define joined_bySet(l,r)       SetP   (dbGetObject (l), 8, dbGetObject (r), NO_PARAMETER)
 #define joined_byUnSet(l,r)     UnSet  (dbGetObject (l), 8, dbGetObject (r))
-#define joined_byGet(l,r)       GetP   (dbGetObject (l), 8, dbGetObject (r))
 
-#define Relation_uses      9
-#define usesP(l,r,p,c)     AskP   (dbGetObject (l), 9, dbGetObject (r), p, c)
-#define uses(l,r)          AskP   (dbGetObject (l), 9, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define usesAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 9, BuildObjectList); }
-#define usesSetP(l,r,p)    SetP   (dbGetObject (l), 9, dbGetObject (r), p)
-#define usesSet(l,r)       SetP   (dbGetObject (l), 9, dbGetObject (r), NO_PARAMETER)
-#define usesUnSet(l,r)     UnSet  (dbGetObject (l), 9, dbGetObject (r))
-#define usesGet(l,r)       GetP   (dbGetObject (l), 9, dbGetObject (r))
+//#define Relation_uses      9
 
-#define Relation_livesIn      10
-#define livesInP(l,r,p,c)     AskP   (dbGetObject (l), 10, dbGetObject (r), p, c)
+//#define Relation_livesIn      10
 #define livesIn(l,r)          AskP   (dbGetObject (l), 10, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define livesInAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 10, BuildObjectList); }
-#define livesInSetP(l,r,p)    SetP   (dbGetObject (l), 10, dbGetObject (r), p)
 #define livesInSet(l,r)       SetP   (dbGetObject (l), 10, dbGetObject (r), NO_PARAMETER)
 #define livesInUnSet(l,r)     UnSet  (dbGetObject (l), 10, dbGetObject (r))
-#define livesInGet(l,r)       GetP   (dbGetObject (l), 10, dbGetObject (r))
 
-#define Relation_break      11
-#define breakP(l,r,p,c)     AskP   (dbGetObject (l), 11, dbGetObject (r), p, c)
+//#define Relation_break      11
 #define break_(l,r)         AskP   (dbGetObject (l), 11, dbGetObject (r), NO_PARAMETER, CMP_NO)
 #define breakAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 11, BuildObjectList); }
-#define breakSetP(l,r,p)    SetP   (dbGetObject (l), 11, dbGetObject (r), p)
-#define breakSet(l,r)       SetP   (dbGetObject (l), 11, dbGetObject (r), NO_PARAMETER)
-#define breakUnSet(l,r)     UnSet  (dbGetObject (l), 11, dbGetObject (r))
 #define breakGet(l,r)       GetP   (dbGetObject (l), 11, dbGetObject (r))
 
-#define Relation_hurt      12
-#define hurtP(l,r,p,c)     AskP   (dbGetObject (l), 12, dbGetObject (r), p, c)
-#define hurt(l,r)          AskP   (dbGetObject (l), 12, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define hurtAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 12, BuildObjectList); }
-#define hurtSetP(l,r,p)    SetP   (dbGetObject (l), 12, dbGetObject (r), p)
-#define hurtSet(l,r)       SetP   (dbGetObject (l), 12, dbGetObject (r), NO_PARAMETER)
-#define hurtUnSet(l,r)     UnSet  (dbGetObject (l), 12, dbGetObject (r))
+//#define Relation_hurt      12
 #define hurtGet(l,r)       GetP   (dbGetObject (l), 12, dbGetObject (r))
 
-#define Relation_sound      13
-#define soundP(l,r,p,c)     AskP   (dbGetObject (l), 13, dbGetObject (r), p, c)
-#define sound(l,r)          AskP   (dbGetObject (l), 13, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define soundAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 13, BuildObjectList); }
-#define soundSetP(l,r,p)    SetP   (dbGetObject (l), 13, dbGetObject (r), p)
-#define soundSet(l,r)       SetP   (dbGetObject (l), 13, dbGetObject (r), NO_PARAMETER)
-#define soundUnSet(l,r)     UnSet  (dbGetObject (l), 13, dbGetObject (r))
+//#define Relation_sound      13
 #define soundGet(l,r)       GetP   (dbGetObject (l), 13, dbGetObject (r))
 
-#define Relation_opens      14
-#define opensP(l,r,p,c)     AskP   (dbGetObject (l), 14, dbGetObject (r), p, c)
-#define opens(l,r)          AskP   (dbGetObject (l), 14, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define opensAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 14, BuildObjectList); }
-#define opensSetP(l,r,p)    SetP   (dbGetObject (l), 14, dbGetObject (r), p)
-#define opensSet(l,r)       SetP   (dbGetObject (l), 14, dbGetObject (r), NO_PARAMETER)
-#define opensUnSet(l,r)     UnSet  (dbGetObject (l), 14, dbGetObject (r))
+//#define Relation_opens      14
 #define opensGet(l,r)       GetP   (dbGetObject (l), 14, dbGetObject (r))
 
-#define Relation_toolRequires      15
-#define toolRequiresP(l,r,p,c)     AskP   (dbGetObject (l), 15, dbGetObject (r), p, c)
-#define toolRequires(l,r)          AskP   (dbGetObject (l), 15, dbGetObject (r), NO_PARAMETER, CMP_NO)
+//#define Relation_toolRequires      15
 #define toolRequiresAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 15, BuildObjectList); }
-#define toolRequiresSetP(l,r,p)    SetP   (dbGetObject (l), 15, dbGetObject (r), p)
-#define toolRequiresSet(l,r)       SetP   (dbGetObject (l), 15, dbGetObject (r), NO_PARAMETER)
-#define toolRequiresUnSet(l,r)     UnSet  (dbGetObject (l), 15, dbGetObject (r))
 #define toolRequiresGet(l,r)       GetP   (dbGetObject (l), 15, dbGetObject (r))
 
 #define Relation_taxi      16
-#define taxiP(l,r,p,c)     AskP   (dbGetObject (l), 16, dbGetObject (r), p, c)
-#define taxi(l,r)          AskP   (dbGetObject (l), 16, dbGetObject (r), NO_PARAMETER, CMP_NO)
 #define taxiAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 16, BuildObjectList); }
-#define taxiSetP(l,r,p)    SetP   (dbGetObject (l), 16, dbGetObject (r), p)
 #define taxiSet(l,r)       SetP   (dbGetObject (l), 16, dbGetObject (r), NO_PARAMETER)
 #define taxiUnSet(l,r)     UnSet  (dbGetObject (l), 16, dbGetObject (r))
-#define taxiGet(l,r)       GetP   (dbGetObject (l), 16, dbGetObject (r))
 
-#define Relation_learned      17
-#define learnedP(l,r,p,c)     AskP   (dbGetObject (l), 17, dbGetObject (r), p, c)
+//#define Relation_learned      17
 #define learned(l,r)          AskP   (dbGetObject (l), 17, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define learnedAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 17, BuildObjectList); }
-#define learnedSetP(l,r,p)    SetP   (dbGetObject (l), 17, dbGetObject (r), p)
-#define learnedSet(l,r)       SetP   (dbGetObject (l), 17, dbGetObject (r), NO_PARAMETER)
-#define learnedUnSet(l,r)     UnSet  (dbGetObject (l), 17, dbGetObject (r))
 #define learnedGet(l,r)       GetP   (dbGetObject (l), 17, dbGetObject (r))
-
-#define Relation_remember      18
-#define rememberP(l,r,p,c)     AskP   (dbGetObject (l), 18, dbGetObject (r), p, c)
-#define remember(l,r)          AskP   (dbGetObject (l), 18, dbGetObject (r), NO_PARAMETER, CMP_NO)
+#define learnedSetP(l,r,p)    SetP   (dbGetObject (l), 17, dbGetObject (r), p)
+#define learnedUnSet(l,r)     UnSet  (dbGetObject (l), 17, dbGetObject (r))
+	
+//#define Relation_remember      18
 #define rememberAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 18, BuildObjectList); }
-#define rememberSetP(l,r,p)    SetP   (dbGetObject (l), 18, dbGetObject (r), p)
 #define rememberSet(l,r)       SetP   (dbGetObject (l), 18, dbGetObject (r), NO_PARAMETER)
 #define rememberUnSet(l,r)     UnSet  (dbGetObject (l), 18, dbGetObject (r))
-#define rememberGet(l,r)       GetP   (dbGetObject (l), 18, dbGetObject (r))
 
-#define Relation_hasLootBag      19
-#define hasLootBagP(l,r,p,c)     AskP   (dbGetObject (l), 19, dbGetObject (r), p, c)
+//#define Relation_hasLootBag      19
 #define hasLootBag(l,r)          AskP   (dbGetObject (l), 19, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define hasLootBagAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 19, BuildObjectList); }
-#define hasLootBagSetP(l,r,p)    SetP   (dbGetObject (l), 19, dbGetObject (r), p)
 #define hasLootBagSet(l,r)       SetP   (dbGetObject (l), 19, dbGetObject (r), NO_PARAMETER)
 #define hasLootBagUnSet(l,r)     UnSet  (dbGetObject (l), 19, dbGetObject (r))
-#define hasLootBagGet(l,r)       GetP   (dbGetObject (l), 19, dbGetObject (r))
 
-#define Relation_PersonWorksHere      20
-#define PersonWorksHereP(l,r,p,c)     AskP   (dbGetObject (l), 20, dbGetObject (r), p, c)
+//#define Relation_PersonWorksHere      20
 #define PersonWorksHere(l,r)          AskP   (dbGetObject (l), 20, dbGetObject (r), NO_PARAMETER, CMP_NO)
-#define PersonWorksHereAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 20, BuildObjectList); }
-#define PersonWorksHereSetP(l,r,p)    SetP   (dbGetObject (l), 20, dbGetObject (r), p)
-#define PersonWorksHereSet(l,r)       SetP   (dbGetObject (l), 20, dbGetObject (r), NO_PARAMETER)
-#define PersonWorksHereUnSet(l,r)     UnSet  (dbGetObject (l), 20, dbGetObject (r))
-#define PersonWorksHereGet(l,r)       GetP   (dbGetObject (l), 20, dbGetObject (r))
-
 
 enum LootNameE { Kein_Name, Eine_Zigarrenkiste, Ein_Tiefgekuehlter_Hummer,
                  Eine_Cartier_Uhr, Ein_Sanskrit_Buch, Ein_Autogramm_von_Oscar_Wilde,
@@ -526,34 +422,15 @@ public:
 #define DB_tcBuild_OFFSET        500000
 #define DB_tcBuild_SIZE          10000
 
-
-#define Relation_isGuardedby      500003
-#define isGuardedbyP(l,r,p,c)     AskP   (dbGetObject (l), 500003, dbGetObject (r), p, c)
-#define isGuardedby(l,r)          AskP   (dbGetObject (l), 500003, dbGetObject (r), NO_PARAMETER, CMP_NO)
+//#define Relation_isGuardedby      500003
 #define isGuardedbyAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 500003, BuildObjectList); }
-#define isGuardedbySetP(l,r,p)    SetP   (dbGetObject (l), 500003, dbGetObject (r), p)
-#define isGuardedbySet(l,r)       SetP   (dbGetObject (l), 500003, dbGetObject (r), NO_PARAMETER)
-#define isGuardedbyUnSet(l,r)     UnSet  (dbGetObject (l), 500003, dbGetObject (r))
 #define isGuardedbyGet(l,r)       GetP   (dbGetObject (l), 500003, dbGetObject (r))
-
-#define Relation_startsWith      500001
-#define startsWithP(l,r,p,c)     AskP   (dbGetObject (l), 500001, dbGetObject (r), p, c)
-#define startsWith(l,r)          AskP   (dbGetObject (l), 500001, dbGetObject (r), NO_PARAMETER, CMP_NO)
+	
+//#define Relation_startsWith      500001
 #define startsWithAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 500001, BuildObjectList); }
-#define startsWithSetP(l,r,p)    SetP   (dbGetObject (l), 500001, dbGetObject (r), p)
-#define startsWithSet(l,r)       SetP   (dbGetObject (l), 500001, dbGetObject (r), NO_PARAMETER)
-#define startsWithUnSet(l,r)     UnSet  (dbGetObject (l), 500001, dbGetObject (r))
-#define startsWithGet(l,r)       GetP   (dbGetObject (l), 500001, dbGetObject (r))
-
-#define Relation_consistsOf      500002
-#define consistsOfP(l,r,p,c)     AskP   (dbGetObject (l), 500002, dbGetObject (r), p, c)
-#define consistsOf(l,r)          AskP   (dbGetObject (l), 500002, dbGetObject (r), NO_PARAMETER, CMP_NO)
+	
+//#define Relation_consistsOf      500002
 #define consistsOfAll(l,f,t)     { SetObjectListAttr (f,t); AskAll (dbGetObject (l), 500002, BuildObjectList); }
-#define consistsOfSetP(l,r,p)    SetP   (dbGetObject (l), 500002, dbGetObject (r), p)
-#define consistsOfSet(l,r)       SetP   (dbGetObject (l), 500002, dbGetObject (r), NO_PARAMETER)
-#define consistsOfUnSet(l,r)     UnSet  (dbGetObject (l), 500002, dbGetObject (r))
-#define consistsOfGet(l,r)       GetP   (dbGetObject (l), 500002, dbGetObject (r))
-
 
 enum RouteE { Innenstadt, Stadt, Landstrasse, Autobahn };
 
