rootfather/scummvm

commit c8ef7528ed15a10e51e1125ffe5b6158e231c3fd

CLUE: Replace forbidden file functions with ScummVM streams
Still hacked together, need to rewrite with File and SaveFile classes later

engines/clue/planing/system.cpp
@@ -93,17 +93,19 @@ void SetActivHandler(struct System *sys, uint32 id) {
 		sys->ActivHandler = NULL;
 }
 
-void SaveSystem(FILE *fh, struct System *sys) {
+void SaveSystem(Common::Stream *fh, struct System *sys) {
 	if (fh) {
-		fprintf(fh, FILE_SYSTEM_ID "\r\n");
+		dskSetLine(fh, FILE_SYSTEM_ID);
 
 		for (struct Handler *h = (struct Handler *) LIST_HEAD(sys->Handlers); NODE_SUCC(h);
-		        h = (struct Handler *) NODE_SUCC(h))
-			fprintf(fh, FILE_HANDLER_ID "\r\n%u\r\n", h->Id);
+		        h = (struct Handler *) NODE_SUCC(h)) {
+			dskSetLine(fh, FILE_HANDLER_ID);
+			dskSetLine_U32(fh, h->Id);
+		}
 	}
 }
 
-LIST *LoadSystem(FILE *fh, struct System *sys) {
+LIST *LoadSystem(Common::Stream *fh, struct System *sys) {
 	LIST *l = txtGoKey(PLAN_TXT, "SYSTEM_GUYS_MISSING_1");
 	bool foundAll = true;
 	uint8 knowsSomebody = 1, handlerNr = 0;
@@ -116,7 +118,7 @@ LIST *LoadSystem(FILE *fh, struct System *sys) {
 			        && strcmp(buffer, FILE_HANDLER_ID) == 0) {
 				uint32 id;
 
-				if (fscanf(fh, "%u\r\n", &id) == 1
+				if (dskGetLine_U32(fh, &id)
 				        && !dbIsObject(id, Object_Police)) {
 					handlerNr++;
 
@@ -228,32 +230,32 @@ bool IsHandlerCleared(struct System *sys) {
 	return false;
 }
 
-void SaveHandler(FILE *fh, struct System *sys, uint32 id) {
+void SaveHandler(Common::Stream *fh, struct System *sys, uint32 id) {
 	struct Handler *h;
 
 	if (fh && sys && (h = FindHandler(sys, id))) {
-		fprintf(fh, FILE_ACTION_LIST_ID "\r\n%u\r\n", id);
+		dskSetLine(fh, FILE_ACTION_LIST_ID);
+		dskSetLine_U32(fh, id);
 
 		for (struct Action *a = (struct Action *) LIST_HEAD(h->Actions); NODE_SUCC(a);
 		        a = (struct Action *) NODE_SUCC(a)) {
-			fprintf(fh, FILE_ACTION_ID "\r\n%hu\r\n%hu\r\n",
-			        a->Type, a->TimeNeeded);
+			dskSetLine(fh, FILE_ACTION_ID);
+			dskSetLine_U16(fh, a->Type);
+			dskSetLine_U16(fh, a->TimeNeeded);
 
 			switch (a->Type) {
 			case ACTION_GO:
-				fprintf(fh, "%hu\r\n",
+				dskSetLine_U16(fh,
 				        ActionData(a, struct ActionGo *)->Direction);
 				break;
 
 			case ACTION_USE:
 			case ACTION_TAKE:
 			case ACTION_DROP:
-				fprintf(fh, "%u\r\n%u\r\n",
-				        ActionData(a, struct ActionUse *)->ToolId, ActionData(a,
-				                struct
-				                ActionUse
-				                *)->
-				        ItemId);
+				dskSetLine_U16(fh,
+				        ActionData(a, struct ActionUse *)->ToolId);
+				dskSetLine_U16(fh,
+				        ActionData(a, struct ActionUse *)->ItemId);
 				break;
 
 			case ACTION_OPEN:
@@ -261,7 +263,7 @@ void SaveHandler(FILE *fh, struct System *sys, uint32 id) {
 			case ACTION_CONTROL:
 			case ACTION_SIGNAL:
 			case ACTION_WAIT_SIGNAL:
-				fprintf(fh, "%u\r\n",
+				dskSetLine_U16(fh,
 				        ActionData(a, struct ActionOpen *)->ItemId);
 				break;
 			}
@@ -269,24 +271,24 @@ void SaveHandler(FILE *fh, struct System *sys, uint32 id) {
 	}
 }
 
-bool LoadHandler(FILE *fh, struct System *sys, uint32 id) {
+bool LoadHandler(Common::Stream *fh, struct System *sys, uint32 id) {
 	if (fh && sys && (FindHandler(sys, id))) {
-		rewind(fh);
+		//rewind(fh);
 
 		char buffer[64];
 		while (dskGetLine(buffer, sizeof(buffer), fh)) {
 			if (strcmp(buffer, FILE_ACTION_LIST_ID) == 0) {
 				uint32 rid;
 
-				if (fscanf(fh, "%u\r\n", &rid) == 1 && id == rid) {
+				if (dskGetLine_U32(fh, &rid) && id == rid) {
 					SetActivHandler(sys, id);
 
 					while (dskGetLine(buffer, sizeof(buffer), fh)
 					        && (strcmp(buffer, FILE_ACTION_ID) == 0)) {
 						uint16 type;
 						uint16 time;
-						fscanf(fh, "%hu\r\n", &type);
-						fscanf(fh, "%hu\r\n", &time);
+						dskGetLine_U16(fh, &type);
+						dskGetLine_U16(fh, &time);
 
 						if (type) {
 							struct Action *a = InitAction(sys, type, 0L, 0L, time);
@@ -295,17 +297,17 @@ bool LoadHandler(FILE *fh, struct System *sys, uint32 id) {
 
 							switch (type) {
 							case ACTION_GO:
-								fscanf(fh, "%hu\r\n", &value16);
+								dskGetLine_U16(fh, &value16);
 								ActionData(a, struct ActionGo *)->Direction = value16;
 								break;
 
 							case ACTION_USE:
 							case ACTION_TAKE:
 							case ACTION_DROP:
-								fscanf(fh, "%u\r\n", &value32);
+								dskGetLine_U32(fh, &value32);
 								ActionData(a, struct ActionUse *)->ToolId = value32;
 
-								fscanf(fh, "%u\r\n", &value32);
+								dskGetLine_U32(fh, &value32);
 								ActionData(a, struct ActionUse *)->ItemId = value32;
 								break;
 
@@ -314,7 +316,7 @@ bool LoadHandler(FILE *fh, struct System *sys, uint32 id) {
 							case ACTION_CONTROL:
 							case ACTION_WAIT_SIGNAL:
 							case ACTION_SIGNAL:
-								fscanf(fh, "%u\r\n", &value32);
+								dskGetLine_U32(fh, &value32);
 								ActionData(a, struct ActionOpen *)->ItemId = value32;
 								break;
 							}

engines/clue/data/database.cpp
@@ -257,13 +257,13 @@ static uint32 dbGetMemSize(uint32 type) {
 }
 
 static void
-dbRWStdObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, FILE *fp) {
-	void (*U8_RW)(FILE * fp, uint8 * x);
-	void (*S8_RW)(FILE * fp, int8 * x);
-	void (*U16LE_RW)(FILE * fp, uint16 * x);
-	void (*S16LE_RW)(FILE * fp, int16 * x);
-	void (*U32LE_RW)(FILE * fp, uint32 * x);
-	void (*S32LE_RW)(FILE * fp, int32 * x);
+dbRWStdObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, Common::Stream *fp) {
+	void (*U8_RW)(Common::Stream * fp, uint8 * x);
+	void (*S8_RW)(Common::Stream * fp, int8 * x);
+	void (*U16LE_RW)(Common::Stream * fp, uint16 * x);
+	void (*S16LE_RW)(Common::Stream * fp, int16 * x);
+	void (*U32LE_RW)(Common::Stream * fp, uint32 * x);
+	void (*S32LE_RW)(Common::Stream * fp, int32 * x);
 
 	if (RW == 0) {
 		U8_RW = dskRead_U8;
@@ -696,13 +696,13 @@ dbRWStdObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, FIL
 }
 
 static void
-dbRWProfiObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, FILE *fp) {
-	void (*U8_RW)(FILE * fp, uint8 * x);
-	void (*S8_RW)(FILE * fp, int8 * x);
-	void (*U16LE_RW)(FILE * fp, uint16 * x);
-	void (*S16LE_RW)(FILE * fp, int16 * x);
-	void (*U32LE_RW)(FILE * fp, uint32 * x);
-	void (*S32LE_RW)(FILE * fp, int32 * x);
+dbRWProfiObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, Common::Stream *fp) {
+	void (*U8_RW)(Common::Stream * fp, uint8 * x);
+	void (*S8_RW)(Common::Stream * fp, int8 * x);
+	void (*U16LE_RW)(Common::Stream * fp, uint16 * x);
+	void (*S16LE_RW)(Common::Stream * fp, int16 * x);
+	void (*U32LE_RW)(Common::Stream * fp, uint32 * x);
+	void (*S32LE_RW)(Common::Stream * fp, int32 * x);
 
 	if (RW == 0) {
 		U8_RW = dskRead_U8;
@@ -1134,11 +1134,11 @@ dbRWProfiObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, F
 }
 
 static void
-dbRWObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, FILE *fp) {
+dbRWObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, Common::Stream *fp) {
 	int32 start;
 
 
-	start = ftell(fp);
+	start = dskTell(fp);
 
 	if (g_clue->getFeatures() & GF_PROFIDISK) {
 		dbRWProfiObject(obj, RW, type, size, localSize, fp);
@@ -1146,20 +1146,20 @@ dbRWObject(void *obj, int RW, uint32 type, uint32 size, uint32 localSize, FILE *
 		dbRWStdObject(obj, RW, type, size, localSize, fp);
 	}
 
-	if ((uint32)(ftell(fp) - start) != size) {
+	if ((uint32)(dskTell(fp) - start) != size) {
 		ErrorMsg(Disk_Defect, ERROR_MODULE_DATABASE, 1);
 	}
 }
 
 
 uint8 dbLoadAllObjects(const char *fileName, uint16 diskId) {
-	FILE *fh;
+	Common::Stream *fh;
 
-	if ((fh = dskOpen(fileName, "rb"))) {
+	if ((fh = dskOpen(fileName, 0))) {
 		uint32 realNr = 1;
 		struct dbObjectHeader objHd;
 
-		while (!feof(fh)) {
+		while (!dskEOF(fh)) {
 			int ch;
 
 			objHd.nr = 0;
@@ -1205,8 +1205,7 @@ uint8 dbLoadAllObjects(const char *fileName, uint16 diskId) {
 				dbRWObject(obj, 0, objHd.type, objHd.size, localSize, fh);
 			}
 
-			if ((ch = fgetc(fh)) != EOF)
-				ungetc(ch, fh);
+			dskPeek(fh);
 		}
 
 		dskClose(fh);
@@ -1217,12 +1216,12 @@ uint8 dbLoadAllObjects(const char *fileName, uint16 diskId) {
 }
 
 uint8 dbSaveAllObjects(const char *fileName, uint32 offset, uint32 size, uint16 diskId) {
-	FILE *fh;
+	Common::Stream *fh;
 	struct dbObject *obj;
 	uint32 realNr = 1;
 	uint32 dbSize = dbGetObjectCountOfDB(offset, size);
 
-	if ((fh = dskOpen(fileName, "wb"))) {
+	if ((fh = dskOpen(fileName, 1))) {
 		while (realNr <= dbSize) {
 			if ((obj = dbFindRealObject(realNr++, offset, size))) {
 				struct dbObjectHeader objHd;

engines/clue/data/relation.cpp
@@ -203,24 +203,26 @@ void AskAll(KEY leftKey, RELATION id, void (*UseKey)(void *)) {
 
 int SaveRelations(const char *file, uint32 offset, uint32 size, uint16 disk_id) {
 	if (relationsDefBase && DecodeKey) {
-		FILE *fh = dskOpen(file, "wb");
+		Common::Stream *fh = dskOpen(file, 1);
 		if (fh) {
-			fprintf(fh, "%s\r\n", REL_FILE_MARK);
+			dskSetLine(fh, REL_FILE_MARK);
 
 			for (struct relationDef *rd = relationsDefBase; rd; rd = rd->rd_next) {
 				if (rd->rd_id > offset) {
 					if (size && (rd->rd_id > offset + size))
 						continue;
 
-					fprintf(fh, "%s\r\n", REL_TABLE_MARK);
-					fprintf(fh, "%u\r\n", rd->rd_id);
+					dskSetLine(fh, REL_TABLE_MARK);
+					dskSetLine_U32(fh, rd->rd_id);
 
 					for (struct relation *r = rd->rd_relationsTable; r; r = r->r_next) {
 						char left[256];
 						char right[256];
 						strcpy(left, DecodeKey(r->r_leftKey));
 						strcpy(right, DecodeKey(r->r_rightKey));
-						fprintf(fh, "%s\r\n%s\r\n%u\r\n", left, right, r->r_parameter);
+						dskSetLine(fh, left);
+						dskSetLine(fh, right);
+						dskSetLine_U32(fh, r->r_parameter);
 					}
 				}
 			}
@@ -244,16 +246,16 @@ bool LoadRelations(const char *file, uint16 disk_id) {
 	right[0] = '\0';
 
 	if (EncodeKey) {
-		FILE *fh = dskOpen(file, "rb");
+		Common::Stream *fh = dskOpen(file, 0);
 		if (fh) {
 			dskGetLine(buffer, sizeof(buffer), fh);
 
 			if (strcmp(buffer, REL_FILE_MARK) == 0) {
 				dskGetLine(buffer, sizeof(buffer), fh);
 
-				while (!feof(fh) && strcmp(buffer, REL_TABLE_MARK) == 0) {
+				while (!dskEOF(fh) && strcmp(buffer, REL_TABLE_MARK) == 0) {
 					RELATION rd;
-					fscanf(fh, "%u\r\n", &rd);
+					dskGetLine_U32(fh, &rd);
 
 					bool goOn = false;
 					if (FindRelation(rd))
@@ -278,7 +280,7 @@ bool LoadRelations(const char *file, uint16 disk_id) {
 								break;
 
 							PARAMETER parameter;
-							if (fscanf(fh, "%u\r\n", &parameter) != 1)
+							if (!dskGetLine_U32(fh, &parameter))
 								break;
 
 							if (!SetP(EncodeKey(left), rd, EncodeKey(right), parameter)) {

engines/clue/disk/disk.cpp
@@ -13,6 +13,9 @@
 #include "clue/disk/disk.h"
 #include "clue/disk/disk_e.h"
 
+// HACK: Replace with proper streams
+#include "backends/fs/stdiostream.h"
+
 namespace Clue {
 
 char RootPathName[DSK_PATH_MAX];
@@ -28,10 +31,16 @@ char *dskGetRootPath(char *result) {
 }
 #endif
 
-FILE *dskOpen(const char *Pathname, const char *Mode) {
-	DebugMsg(ERR_DEBUG, ERROR_MODULE_DISK, "Opening :%s (%s)", Pathname, Mode);
+Common::Stream *dskOpen(const char *Pathname, int RW) {
+	Common::Stream *fp = NULL;
+	if (RW == 0) {
+		DebugMsg(ERR_DEBUG, ERROR_MODULE_DISK, "Reading :%s", Pathname);
+		fp = StdioStream::makeFromPath(Pathname, false);
+	} else {
+		DebugMsg(ERR_DEBUG, ERROR_MODULE_DISK, "Writing :%s", Pathname);
+		fp = StdioStream::makeFromPath(Pathname, true);
+	}
 
-	FILE *fp = fopen(Pathname, Mode);
 	if (!fp)
 		DebugMsg(ERR_ERROR, ERROR_MODULE_DISK, "Open :%s", Pathname);
 
@@ -40,14 +49,14 @@ FILE *dskOpen(const char *Pathname, const char *Mode) {
 
 void *dskLoad(const char *Pathname) {
 
-	FILE *fp = dskOpen(Pathname, "rb");
+	Common::Stream *fp = dskOpen(Pathname, 0);
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
 	if (fp) {
 		size_t nread;
 		size_t pos = 0;
 		size_t size = BUFSIZ;
 		uint8 *ptr = (uint8 *)malloc(size);
-
-		while ((nread = fread(ptr + pos, 1, BUFSIZ, fp)) == BUFSIZ) {
+		while ((nread = stream->read(ptr + pos, BUFSIZ)) == BUFSIZ) {
 			pos  += nread;
 			ptr   = (uint8 *)realloc(ptr, size + BUFSIZ);
 			size += nread;
@@ -64,7 +73,7 @@ void *dskLoad(const char *Pathname) {
 
 #if 0
 void dskSave(const char *Pathname, void *src, size_t size) {
-	FILE *fp = dskOpen(Pathname, "wb");
+	Common::Stream *fp = dskOpen(Pathname, 1);
 	if (fp) {
 		dskWrite(fp, src, size);
 		dskClose(fp);
@@ -145,116 +154,127 @@ size_t dskFileLength(const char *Pathname) {
 	return status.st_size;
 }
 
-void dskClose(FILE *fp) {
+void dskClose(Common::Stream *fp) {
 	if (fp) {
-		fclose(fp);
+		delete fp;
 	}
 }
 
-void dskWrite(FILE *fp, void *src, size_t size) {
-	if (fwrite(src, 1, size, fp) != size) {
+void dskWrite(Common::Stream *fp, void *src, uint32 size) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream *>(fp);
+	stream->write(src, size);
+	if (stream->err()) {
 		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_WRITE_FAILED);
 	}
 }
 
-void dskWrite_U8(FILE *fp, uint8 *x) {
-	uint8 tmp = *x;
-	dskWrite(fp, &tmp, sizeof(tmp));
+void dskWrite_U8(Common::Stream *fp, uint8 *x) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream*>(fp);
+	stream->writeByte(*x);
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_WRITE_FAILED);
+	}
 }
 
-void dskWrite_S8(FILE *fp, int8 *x) {
-	int8 tmp = *x;
-	dskWrite(fp, &tmp, sizeof(tmp));
+void dskWrite_S8(Common::Stream *fp, int8 *x) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream *>(fp);
+	stream->writeSByte(*x);
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_WRITE_FAILED);
+	}
 }
 
-void dskWrite_U16LE(FILE *fp, uint16 *x) {
-	uint8 tmp[2];
-
-	tmp[0] = (uint8)((*x) & 0xff);
-	tmp[1] = (uint8)((*x >> 8) & 0xff);
-	dskWrite(fp, &tmp, sizeof(tmp));
+void dskWrite_U16LE(Common::Stream *fp, uint16 *x) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream *>(fp);
+	stream->writeUint16LE(*x);
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_WRITE_FAILED);
+	}
 }
 
-void dskWrite_S16LE(FILE *fp, int16 *x) {
-	uint8 tmp[2];
-
-	tmp[0] = (uint8)((*x) & 0xff);
-	tmp[1] = (uint8)((*x >> 8) & 0xff);
-	dskWrite(fp, &tmp, sizeof(tmp));
+void dskWrite_S16LE(Common::Stream *fp, int16 *x) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream *>(fp);
+	stream->writeSint16LE(*x);
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_WRITE_FAILED);
+	}
 }
 
-void dskWrite_U32LE(FILE *fp, uint32 *x) {
-	uint8 tmp[4];
-
-	tmp[0] = (uint8)((*x) & 0xff);
-	tmp[1] = (uint8)((*x >> 8) & 0xff);
-	tmp[2] = (uint8)((*x >> 16) & 0xff);
-	tmp[3] = (uint8)((*x >> 24) & 0xff);
-	dskWrite(fp, &tmp, sizeof(tmp));
+void dskWrite_U32LE(Common::Stream *fp, uint32 *x) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream *>(fp);
+	stream->writeUint32LE(*x);
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_WRITE_FAILED);
+	}
 }
 
-void dskWrite_S32LE(FILE *fp, int32 *x) {
-	uint8 tmp[4];
-
-	tmp[0] = (uint8)((*x) & 0xff);
-	tmp[1] = (uint8)((*x >> 8) & 0xff);
-	tmp[2] = (uint8)((*x >> 16) & 0xff);
-	tmp[3] = (uint8)((*x >> 24) & 0xff);
-	dskWrite(fp, &tmp, sizeof(tmp));
+void dskWrite_S32LE(Common::Stream *fp, int32 *x) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream *>(fp);
+	stream->writeSint32LE(*x);
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_WRITE_FAILED);
+	}
 }
 
-void dskRead(FILE *fp, void *dest, size_t size) {
-	if (fread(dest, 1, size, fp) != size) {
+void dskRead(Common::Stream *fp, void *dest, size_t size) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	stream->read(dest, size);
+	if (stream->err()) {
 		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_READ_FAILED);
 	}
 }
 
-void dskRead_U8(FILE *fp, uint8 *x) {
-	uint8 tmp;
-
-	dskRead(fp, &tmp, sizeof(tmp));
-	*x = tmp;
+void dskRead_U8(Common::Stream *fp, uint8 *x) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	*x = stream->readByte();
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_READ_FAILED);
+	}
 }
 
-void dskRead_S8(FILE *fp, int8 *x) {
-	int8 tmp;
-
-	dskRead(fp, &tmp, sizeof(tmp));
-	*x = tmp;
+void dskRead_S8(Common::Stream *fp, int8 *x) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	*x = stream->readSByte();
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_READ_FAILED);
+	}
 }
 
-void dskRead_U16LE(FILE *fp, uint16 *x) {
-	uint8 tmp[2];
-
-	dskRead(fp, &tmp, sizeof(tmp));
-	*x = (uint16)((uint16) tmp[0] | ((uint16) tmp[1] << 8));
+void dskRead_U16LE(Common::Stream *fp, uint16 *x) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	*x = stream->readUint16LE();
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_READ_FAILED);
+	}
 }
 
-void dskRead_S16LE(FILE *fp, int16 *x) {
-	uint8 tmp[2];
-
-	dskRead(fp, &tmp, sizeof(tmp));
-	*x = (int16)((uint16) tmp[0] | ((uint16) tmp[1] << 8));
+void dskRead_S16LE(Common::Stream *fp, int16 *x) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	*x = stream->readSint16LE();
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_READ_FAILED);
+	}
 }
 
-void dskRead_U32LE(FILE *fp, uint32 *x) {
-	uint8 tmp[4];
-
-	dskRead(fp, &tmp, sizeof(tmp));
-	*x = (uint32)((uint32) tmp[0] | ((uint32) tmp[1] << 8)
-	              | ((uint32) tmp[2] << 16) | ((uint32) tmp[3] << 24));
+void dskRead_U32LE(Common::Stream *fp, uint32 *x) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	*x = stream->readUint32LE();
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_READ_FAILED);
+	}
 }
 
-void dskRead_S32LE(FILE *fp, int32 *x) {
-	uint8 tmp[4];
-
-	dskRead(fp, &tmp, sizeof(tmp));
-	*x = (int32)((uint32) tmp[0] | ((uint32) tmp[1] << 8)
-	             | ((uint32) tmp[2] << 16) | ((uint32) tmp[3] << 24));
+void dskRead_S32LE(Common::Stream *fp, int32 *x) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	*x = stream->readSint32LE();
+	if (stream->err()) {
+		ErrorMsg(Disk_Defect, ERROR_MODULE_DISK, ERR_DISK_READ_FAILED);
+	}
 }
 
-bool dskGetLine(char *s, int size, FILE *fp) {
-	if (fgets(s, size, fp)) {
+bool dskGetLine(char *s, int size, Common::Stream *fp) {
+	Common::SeekableReadStream *stream = dynamic_cast<Common::SeekableReadStream *>(fp);
+	if (stream->readLine(s, size)) {
 		char *p;
 		if (p = strrchr(s, '\n'))
 			*p = '\0';
@@ -265,6 +285,62 @@ bool dskGetLine(char *s, int size, FILE *fp) {
 		return true;
 	}
 	return false;
+}
+
+void dskSetLine(Common::Stream *fp, const char *s) {
+	Common::WriteStream *stream = dynamic_cast<Common::WriteStream *>(fp);
+	stream->writeString(s);
+	stream->writeString("\r\n");
+}
+
+bool dskGetLine_U16(Common::Stream *fp, uint16 *x) {
+	Common::SeekableReadStream *stream = dynamic_cast<Common::SeekableReadStream *>(fp);
+	Common::String line = stream->readLine();
+	if (stream->err() || stream->eos()) {
+		return false;
+	}
+	*x = atol(line.c_str());
+	return true;
+}
+
+bool dskGetLine_U32(Common::Stream *fp, uint32 *x) {
+	Common::SeekableReadStream *stream = dynamic_cast<Common::SeekableReadStream *>(fp);
+	Common::String line = stream->readLine();
+	if (stream->err() || stream->eos()) {
+		return false;
+	}
+	*x = atol(line.c_str());
+	return true;
+}
+
+void dskSetLine_U16(Common::Stream *fp, uint16 x) {
+	char buf[BUFSIZ];
+	sprintf(buf, "%hu", x);
+	dskSetLine(fp, buf);
+}
+
+void dskSetLine_U32(Common::Stream *fp, uint32 x) {
+	char buf[BUFSIZ];
+	sprintf(buf, "%u", x);
+	dskSetLine(fp, buf);
+}
+
+bool dskEOF(Common::Stream *fp) {
+	Common::ReadStream *stream = dynamic_cast<Common::ReadStream *>(fp);
+	return stream->eos();
+}
+
+int32 dskTell(Common::Stream *fp) {
+	Common::SeekableReadStream *stream = dynamic_cast<Common::SeekableReadStream *>(fp);
+	return stream->pos();
+}
+
+void dskPeek(Common::Stream *fp) {
+	Common::SeekableReadStream *stream = dynamic_cast<Common::SeekableReadStream *>(fp);
+	stream->readByte();
+	if (!stream->eos()) {
+		stream->seek(-1, SEEK_CUR);
+	}
 }
 
 } // End of namespace Clue

engines/clue/disk/disk.h
@@ -13,6 +13,10 @@
 
 #include "clue/theclou.h"
 
+namespace Common {
+class Stream;
+}
+
 namespace Clue {
 
 #define DSK_PATH_MAX 512
@@ -32,31 +36,40 @@ extern void dskSetRootPath(const char *newRootPath);
 
 void *dskLoad(const char *Pathname);
 bool dskBuildPathName(DiskCheckE check, const char *Directory, const char *Filename, char *Result);
-void dskClose(FILE *fp);
-void dskWrite(FILE *fp, void *src, size_t size);
+void dskClose(Common::Stream *fp);
+void dskWrite(Common::Stream *fp, void *src, uint32 size);
 
-void dskWrite_U8(FILE *fp, uint8 *x);
-void dskWrite_S8(FILE *fp, int8 *x);
-void dskWrite_U16LE(FILE *fp, uint16 *x);
-void dskWrite_S16LE(FILE *fp, int16 *x);
-void dskWrite_U32LE(FILE *fp, uint32 *x);
-void dskWrite_S32LE(FILE *fp, int32 *x);
+void dskWrite_U8(Common::Stream *fp, uint8 *x);
+void dskWrite_S8(Common::Stream *fp, int8 *x);
+void dskWrite_U16LE(Common::Stream *fp, uint16 *x);
+void dskWrite_S16LE(Common::Stream *fp, int16 *x);
+void dskWrite_U32LE(Common::Stream *fp, uint32 *x);
+void dskWrite_S32LE(Common::Stream *fp, int32 *x);
 
-void dskRead(FILE *fp, void *dest, size_t size);
+void dskRead(Common::Stream *fp, void *dest, uint32 size);
 
-void dskRead_U8(FILE *fp, uint8 *x);
-void dskRead_S8(FILE *fp, int8 *x);
-void dskRead_U16LE(FILE *fp, uint16 *x);
-void dskRead_S16LE(FILE *fp, int16 *x);
-void dskRead_U32LE(FILE *fp, uint32 *x);
-void dskRead_S32LE(FILE *fp, int32 *x);
+void dskRead_U8(Common::Stream *fp, uint8 *x);
+void dskRead_S8(Common::Stream *fp, int8 *x);
+void dskRead_U16LE(Common::Stream *fp, uint16 *x);
+void dskRead_S16LE(Common::Stream *fp, int16 *x);
+void dskRead_U32LE(Common::Stream *fp, uint32 *x);
+void dskRead_S32LE(Common::Stream *fp, int32 *x);
 
 
 size_t dskFileLength(const char *Pathname);
 
-FILE *dskOpen(const char *Pathname, const char *Mode);
+Common::Stream *dskOpen(const char *Pathname, int RW);
 
-extern bool dskGetLine(char *s, int size, FILE *fp);
+extern bool dskGetLine(char *s, int size, Common::Stream *fp);
+extern void dskSetLine(Common::Stream *fp, const char *s);
+extern bool dskGetLine_U16(Common::Stream *fp, uint16 *x);
+extern bool dskGetLine_U32(Common::Stream *fp, uint32 *x);
+extern void dskSetLine_U16(Common::Stream *fp, uint16 x);
+extern void dskSetLine_U32(Common::Stream *fp, uint32 x);
+
+bool dskEOF(Common::Stream *fp);
+int32 dskTell(Common::Stream *fp);
+void dskPeek(Common::Stream *fp);
 
 } // End of namespace Clue
 

engines/clue/gameplay/gp.cpp
@@ -31,7 +31,7 @@ void InitSceneInfo(void);
 void FreeSceneInfo(void);
 
 void PrepareStory(const char *filename);
-void LoadSceneforStory(struct NewScene *dest, FILE *file);
+void LoadSceneforStory(struct NewScene *dest, Common::Stream *file);
 
 void InitConditions(struct Scene *scene, struct NewScene *ns);
 void FreeConditions(struct Scene *scene);
@@ -365,7 +365,7 @@ void PrepareStory(const char *filename) {
 	dskBuildPathName(DISK_CHECK_FILE, DATA_DIRECTORY, filename, pathname);
 
 	/* StoryHeader laden ! */
-	FILE *file = dskOpen(pathname, "rb");
+	Common::Stream *file = dskOpen(pathname, 0);
 
 	struct StoryHeader SH;
 	dskRead(file, SH.StoryName, sizeof(SH.StoryName));
@@ -493,7 +493,7 @@ void FreeConditions(struct Scene *scene) {
 	}
 }
 
-void LoadSceneforStory(struct NewScene *dest, FILE *file) {
+void LoadSceneforStory(struct NewScene *dest, Common::Stream *file) {
 	dskRead_U32LE(file, &dest->EventNr);
 
 	dskRead(file, dest->SceneName, sizeof(dest->SceneName));

engines/clue/gameplay/loadsave.cpp
@@ -227,13 +227,13 @@ void tcRefreshAfterLoad(bool loaded) {
 
 bool tcSaveChangesInScenes(const char *fileName) {
 	bool back = false;
-	FILE *file = dskOpen(fileName, "wb");
+	Common::Stream *file = dskOpen(fileName, 1);
 	if (file) {
-		fprintf(file, "%u\r\n", film->EnabledChoices);
+		dskSetLine_U32(file, film->EnabledChoices);
 
 		for (uint32 i = 0; i < film->AmountOfScenes; i++) {
-			fprintf(file, "%u\r\n", film->gameplay[i].EventNr);
-			fprintf(file, "%hu\r\n", film->gameplay[i].Geschehen);
+			dskSetLine_U32(file, film->gameplay[i].EventNr);
+			dskSetLine_U16(file, film->gameplay[i].Geschehen);
 		}
 
 		dskClose(file);
@@ -245,17 +245,17 @@ bool tcSaveChangesInScenes(const char *fileName) {
 
 bool tcLoadChangesInScenes(const char *fileName) {
 	bool back = true;
-	FILE *file = dskOpen(fileName, "rb");
+	Common::Stream *file = dskOpen(fileName, 0);
 	if (file) {
 		uint32 choice;
-		fscanf(file, "%u\r\n", &choice);
+		dskGetLine_U32(file, &choice);
 		SetEnabledChoices(choice);
 
 		for (uint32 i = 0; i < film->AmountOfScenes; i++) {
 			uint32 eventNr;
 			uint16 count;
-			fscanf(file, "%u\r\n", &eventNr);
-			fscanf(file, "%hu\r\n", &count);
+			dskGetLine_U32(file, &eventNr);
+			dskGetLine_U16(file, &count);
 
 			struct Scene *sc = GetScene(eventNr);
 			if (sc)

engines/clue/gfx/gfx.cpp
@@ -1307,17 +1307,17 @@ void ShowIntro(void) {
 		if (setup.CDAudio)
 			CDROM_StopAudioTrack();
 
-		if (!dskBuildPathName(DISK_CHECK_FILE, "intropix", names[anims], pathName))
+		if (!dskBuildPathName(DISK_CHECK_FILE, INTRO_DIRECTORY, names[anims], pathName))
 			continue;
 
-		FILE *fp = dskOpen(pathName, "rb");
-		if (fp) {
+		Common::Stream *fp = dskOpen(pathName, 0);
+		if (!fp) {
 			XMSOffset = 0;
 
-			char head[4];
-			fread(&head[0], 1, 4, fp);
+			uint32 head;
+			dskRead_U32LE(fp, &head);
 			size_t size;
-			fread(&size, 1, 4, fp);
+			dskRead_U32LE(fp, &size);
 			size_t rsize = Amg2Pc(size);
 
 			dskRead(fp, XMSHandle, rsize);

commit 0fc46bf392affa85759dcec3073c89ad69901c02

CLUE: Fix a couple of U16/U32 issues introduced in c8ef7528ed15a10e51e1125ffe5b6158e231c3fd

engines/clue/planing/system.cpp
@@ -230,32 +230,32 @@ void SaveHandler(Common::Stream *fh, System *sys, uint32 id) {
 			break;
 
 		case ACTION_USE:
-			dskSetLine_U16(fh, ((ActionUseNode *)a)->ToolId);
-			dskSetLine_U16(fh, ((ActionUseNode *)a)->ItemId);
+			dskSetLine_U32(fh, ((ActionUseNode *)a)->ToolId);
+			dskSetLine_U32(fh, ((ActionUseNode *)a)->ItemId);
 			break;
 		case ACTION_TAKE:
-			dskSetLine_U16(fh, ((ActionTakeNode *)a)->LootId);
-			dskSetLine_U16(fh, ((ActionTakeNode *)a)->ItemId);
+			dskSetLine_U32(fh, ((ActionTakeNode *)a)->LootId);
+			dskSetLine_U32(fh, ((ActionTakeNode *)a)->ItemId);
 			break;
 		case ACTION_DROP:
-			dskSetLine_U16(fh, ((ActionDropNode *)a)->LootId);
-			dskSetLine_U16(fh, ((ActionDropNode *)a)->ItemId);
+			dskSetLine_U32(fh, ((ActionDropNode *)a)->LootId);
+			dskSetLine_U32(fh, ((ActionDropNode *)a)->ItemId);
 			break;
 
 		case ACTION_OPEN:
-			dskSetLine_U16(fh, ((ActionOpenNode *)a)->ItemId);
+			dskSetLine_U32(fh, ((ActionOpenNode *)a)->ItemId);
 			break;
 		case ACTION_CLOSE:
-			dskSetLine_U16(fh, ((ActionCloseNode *)a)->ItemId);
+			dskSetLine_U32(fh, ((ActionCloseNode *)a)->ItemId);
 			break;
 		case ACTION_CONTROL:
-			dskSetLine_U16(fh, ((ActionControlNode *)a)->ItemId);
+			dskSetLine_U32(fh, ((ActionControlNode *)a)->ItemId);
 			break;
 		case ACTION_SIGNAL:
-			dskSetLine_U16(fh, ((ActionSignalNode *)a)->ReceiverId);
+			dskSetLine_U32(fh, ((ActionSignalNode *)a)->ReceiverId);
 			break;
 		case ACTION_WAIT_SIGNAL:
-			dskSetLine_U16(fh, ((ActionWaitSignalNode *)a)->SenderId);
+			dskSetLine_U32(fh, ((ActionWaitSignalNode *)a)->SenderId);
 			break;
 		}
 	}